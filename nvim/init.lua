8ae3a44588a1ec3fb187dfb9c93634b63de328c6
Matthew Recinto <recinto.matt@gmail.com>
2025-12-14 07:00:27 -0800

    working nvim base config

---
+++ nvim/init.lua
@@ -0,0 +1,14 @@
+-- matthew's config
+--
+
+-- main options
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+
+vim.g.mapleader = " "
+
+print("Loaded: nvim (main)")
+
